b802ce52a16fdd9e2c384aab519eb5f3adf844bb
Răzvan Filea <razvan.filea@gmail.com>
2026-08-23 14:05:00 +0300

    Move PEXT generation to build.rs

--- src/attacks/magic.rs
+++ src/attacks/magic.rs
@@ -1,5 +1,5 @@
 use crate::square::Sq;
 
 
-pub const fn bishop_attacks(sq: Sq) -> u64 {0}
-pub const fn rook_attacks(sq: Sq) -> u64 {0}
+pub const fn bishop_attacks(sq: Sq) -> u64 {todo!()}
+pub const fn rook_attacks(sq: Sq) -> u64 {todo!()}
